bdab201d7f731ce4887b30fd2adc9e6baecf7f1f
hanabi0412 <nguyenkientrung0412@gmail.com>
2019-11-28 19:33:46 +0700

    Sửa database

---
+++ WindowsFormsApp4/Imp/ConnDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp4.Imp
+{
+    public abstract class ConnDB
+    {
+        public static SqlConnection Conn()
+        {
+            // tùy vào mỗi máy nha
+            String connString = "Data Source=localhost,1433;Initial Catalog=milktea;User ID=SA;Password=1234";
+            SqlConnection connection = new SqlConnection(connString);
+            return connection;
+
+        }
+        
+    }
+}
