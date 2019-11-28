4727dd73438d942ea71ac64c7a4af50333438244
hanabi0412 <nguyenkientrung0412@gmail.com>
2019-11-28 14:13:10 +0700

    Them database

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -29,10 +29,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(790, 415);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
         }
 
         #endregion
