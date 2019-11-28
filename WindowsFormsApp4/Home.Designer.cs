941db21995cbe208667f5c230956cc62030e826d
= <=>
2019-11-28 18:28:13 +0700

    test

---
+++ WindowsFormsApp4/Home.Designer.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+
+namespace WindowsFormsApp4
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources =
+                new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream =
+                ((System.Windows.Forms.ImageListStreamer) (resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "35540744_815683571964695_7532884192028262400_n.jpg");
+            this.imageList1.Images.SetKeyName(1, "35553448_815683201964732_6772587990368649216_n.jpg");
+            this.imageList1.Images.SetKeyName(2, "35628993_815683318631387_8478904864150126592_n.jpg");
+            this.imageList1.Images.SetKeyName(3, "35659910_815683518631367_5502823313518362624_n.jpg");
+            this.imageList1.Images.SetKeyName(4, "36372405_824516521081400_7020776399304654848_n.jpg");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.button1);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+    }
+}
