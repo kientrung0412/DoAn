1919edebcf5bd8352a73b0c6c52d8fdd386dc8e6
hanabi0412 <nguyenkientrung0412@gmail.com>
2019-11-29 18:35:17 +0700

    Kiết nối database

--- WindowsFormsApp4/Imp/ConnDB.cs
+++ WindowsFormsApp4/Imp/ConnDB.cs
@@ -7,7 +7,7 @@ namespace WindowsFormsApp4.Imp
     {
         public static SqlConnection Conn()
         {
-            // tùy vào mỗi máy nha
+            // tùy vào mỗi máy nha, mỗi người xe có cổng và tên đang nhập khác nhau
             String connString = "Data Source=localhost,1433;Initial Catalog=milktea;User ID=SA;Password=1234";
             SqlConnection connection = new SqlConnection(connString);
             return connection;
